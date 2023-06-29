7315a52fd0f04c21bfd82705dd26a81a6e2016c4
Cristan Serrano <nanonathan.123@gmail.com>
2023-06-28 21:37:51 -0300

    actualizo servicio para obtener nodos

--- code/modules/Environment.py
+++ code/modules/Environment.py
@@ -1,7 +1,63 @@
-import json
-import os
+import requests
+
+class Environment:
+    def __init__(self):
+        self.base_url = "http://localhost:3000"
+
+    def getNodes(self) -> list:
+        url = f"{self.base_url}/nodos"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return [int(node['id']) for node in response.json()]
+        else:
+            print("Error al obtener los nodos")
+            return []
+
+    def getConnections(self, idNode: str) -> list:
+        url = f'{self.base_url}/nodos/{idNode}/adyacentes'
+
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return [int(adyacente['nodo']['id']) for adyacente in response.json()['adyacentes']]
+            else:
+                print('Error al obtener las conexiones:', response.status_code)
+                return []
+        except requests.exceptions.RequestException as e:
+            print('Error de conexión:', str(e))
+            return []
+
+    def getDistance(self, idNode1: str, idNode2: str) -> int:
+        url = f'{self.base_url}/distancias/{idNode1}/{idNode2}'
+
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return response.json()['distancia']
+            else:
+                print('Error al obtener la distancia:', response.status_code)
+                return -1
+        except requests.exceptions.RequestException as e:
+            print('Error de conexión:', str(e))
+            return -1
+
+    def getCoordinates(self, idNode: str) -> tuple[int, int]:
+        url = f'{self.base_url}/nodos/{idNode}'
+        print("Coordenadas de:", idNode)
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                node_data = response.json()
+                return node_data['coordenada_x'], node_data['coordenada_y']
+            else:
+                print('Error al obtener las coordenadas:', response.status_code)
+                return ()
+        except requests.exceptions.RequestException as e:
+            print('Error de conexión:', str(e))
+            return ()
 
 
+'''
 class Environment:
     def __init__(self) -> None:
         print("Obteniendo datos...")
@@ -34,3 +90,4 @@ class Environment:
 
     def getVertex (self, idNode: str):
         return self.coordenadas[idNode]
+'''
