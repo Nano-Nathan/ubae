6f3347fcf5cb1c941a7e8ad1b8196afd96c7225f
fabriexe <fabricioexe54@gmail.com>
2023-07-18 04:05:46 -0300

    algoritmo genetico, fitness fallando

--- code/main.py
+++ code/main.py
@@ -1,5 +1,15 @@
 from a_star import AStar
+from ag import Agen
+#A = AStar()
+#(A.environment.getConnections(1))
+#print(A.findPath("1", "1000"))
 
+B = Agen()
+
+D=B.findPath(1,1000)
+c=D
+print("___________________________________________________________________________________________")
+print(c)
 A = AStar()
-
+(A.environment.getConnections(1))
 print(A.findPath("1", "1000"))
