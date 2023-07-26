eff7baa4b8fc6dc35e913889d197fc68446a6d6d
fabriexe <fabricioexe54@gmail.com>
2023-07-26 02:15:26 -0300

    Comentarios, fitness arreglado

--- code/ag.py
+++ code/ag.py
@@ -2,9 +2,9 @@ from modules.Environment import Environment
 import math
 import time
 import random
-mutationprob=0.8
-cantdepob=10
-cantdeind=20
+mutationprob=0.6
+cantdein=10
+cantdenodes=20
 iteraciones=50
 class Agen:
     def __init__(self):
@@ -13,32 +13,36 @@ class Agen:
     def findPath(self, startNode, endNode):
         print("Nodo inicial:", startNode)
         print("Nodo final:", endNode)
+        #Guardo las coordenadas del nodo destino
         self.startNode=startNode
         self.endNode=endNode
-        pob=self.generarpoblacion(cantdepob,cantdeind)
-        #Guardo las coordenadas del nodo destino
+        pob=self.generarpoblacion(cantdein,cantdenodes)#genero la primer poblacion
+        
         start = time.time() #Marca el tiempo de inicio
         i=0
         while i<iteraciones:
+            #Las ordeno por menor fitness
             bestpo=sorted(pob,key=lambda pob:pob[0])
+            #selecciono una cantidad reducida
             parent=self.selection(bestpo,4)
+            #de la cantidad reducida hago el cruzamiento
             newchild=self.cruzamiento(parent)
+            #hago la mutacion de los nuevos cruzamientos
             newchildmut=self.mutation(newchild)
             pob=parent+newchildmut
             
             if self.verificacion(pob):
                 print("Camino encontrado.")
                 break
-            print(i)
             i+=1
         timeE = round((time.time() - start) * 10**3, 4)
-        print(self.selection(sorted(pob,key=lambda pob:pob[0]),4))
         print("Tiempo de ejecución:", timeE, "ms") #Muestra el tiempo de ejecucion
-        #print("Estados:", states) #Muestra la cantidad de estados recorridos
-        #print("Costo:", weights[current]) #Muestra el costo de viaje
+        print("Estados:", i) #Muestra la cantidad de estados recorridos
+        print("Costo:", self.caminodist(self.selection(sorted(pob,key=lambda pob:pob[0]),1))) #Muestra el costo de viaje
 
 
-    def generacamino(self,cantdeind,hijo,signode=None):
+    def generacamino(self,cantdenodes,hijo,signode=None):
+        #Si hay mutacion el camino se genera desde donde se corto, sino se crea desde el principio
         if len(hijo)==0:
             node=self.startNode
             hijo.append(f"{node}")
@@ -52,34 +56,39 @@ class Agen:
                     node=i
                     break
         i=0
-        while len(hijo)<cantdeind-2 :
+        #Mientras la cantidad de nodos que sea la muestra del individuo va asignando nodos
+        while len(hijo)<cantdenodes-2 :
             list=(self.environment.getConnections(node))
             addNode= list[random.randint(0,len(list)-1)]
+            #si encuentra el camino retorna
             if addNode==self.endNode:
-                print("llegue")
                 break
             i+=1
+            #Si el individuo no tiene mas conecciones despues de ciertas interacciones vuelve
             if i==5:
                 break
+            #agrega el nodo a la lista
             if not (addNode in hijo):
                 hijo.append(addNode)
                 node=addNode
                 i=0
+        #Agrega el nodo final
         hijo.append(self.endNode)
+        #calcula el fitness
         return self.fitness(hijo)
 
 
-    def individuo(self,cantdeind):
+    def individuo(self,cantdenodes):
         hijo=[]
-        return self.generacamino(cantdeind,hijo)
+        return self.generacamino(cantdenodes,hijo)
 
 
-
+    #Calcula la distancia de los nodos
     def caminodist(self,hijo):
-        fs=0
+        peso=0
         for i in range(len(hijo)-2):
             fs+= self.environment.getDistance(hijo[i],hijo[i+1])
-        hijo=[fs,hijo]
+        hijo=[peso,hijo]
         return hijo
     
 
@@ -89,6 +98,7 @@ class Agen:
         endnode=self.endNode
         ultnodecord=self.environment.getCoordinates(ultnode)
         endnodecord=self.environment.getCoordinates(endnode)
+        #Calcula la distancia entre el ultimo nodo encontrado y el de destino 
         fs=math.sqrt((ultnodecord[0]-endnodecord[0])**2+(ultnodecord[1]-endnodecord[1])**2)
         return [fs,hijo]
 
@@ -101,6 +111,7 @@ class Agen:
         lenp2=len(parent2)
         child=[]
         i=random.randint(1,lenp1-2)
+        #Si comparten un nodo en comun random, se genera el cruzamiento sino retorna y elige 2 padres al azar
         if parent1[i]in parent2:
             for j in range(lenp1):
                 if j<=i and lenp2>j :
@@ -115,12 +126,15 @@ class Agen:
         lenchild=len(child)
         val=random.randint(1,lenchild-2)
         newmutatedchild=[]
+        #de un valor random muta y genera otro camino
         for i in range(val):
             newmutatedchild.append(child[i])
-        return self.generacamino(cantdeind,newmutatedchild,child[val])
+        #tiene el valor del nodo para no repetir el mismo camino
+        return self.generacamino(cantdenodes,newmutatedchild,child[val])
     
     def mutation(self,newchilds):
         mutacion=[]
+        #Por la cantidad de individuos tiene una probabilidad de mutar
         for i in newchilds:
             ruta=i
             if mutationprob>random.random():
@@ -130,25 +144,32 @@ class Agen:
         
         
     def cruzamiento(self,padres):
-        missingchilds=cantdeind-len(padres)
+        #Por la cantidad de individuos que faltan en la poblacion genera individuos
+        missingchilds=cantdenodes-len(padres)
         newchildren=[]
         while len(newchildren)<missingchilds:
+            #selecciona 2 padres random
             parent1,parent2=random.sample(padres,2)
             child=self.rcruzamiento(parent1[1],parent2[1])
+            #si se genera el cruzamiento inserta el individuo
             if child!=False:
                 newchildren.append(self.fitness(child))
         return newchildren
+    
     def verificacion(self,pobl):
+        #verifica si el camino esta conectado
         list=(self.environment.getConnections(self.endNode))
         for i in pobl:
             j=i[1]
             if j[len(pobl[1])-2]in list:
-                print ("llegue")
-    def generarpoblacion (self,maxpop,cantdeind):
+                return True
+            
+    def generarpoblacion (self,maxpop,cantdenodes):
+        #genera la poblacion 
         poblacion=[]
         node=self.startNode
         for i in range(maxpop):
-            poblacion.append(self.individuo(cantdeind))
+            poblacion.append(self.individuo(cantdenodes))
         return poblacion
 
     
